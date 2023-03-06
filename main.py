e5156d9d8aefa7a30e0c537b2beec1e597b9ec90
Python3-8 <pranav.pooruli@gmail.com>
2023-03-06 23:41:04 +0530

    migrated to discord.ext.commands and extracted necessary information in start_game

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
 import discord
+from discord.ext import commands
 import chess
+import pprint
 
 with open('token.txt') as tokenf:
     token = tokenf.read()
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as {bot.user}')
 
 
-@client.event
+@bot.event
 async def on_message(message: discord.Message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
-    if message.content.startswith('/'):
-        await exec_command(message)
+    await bot.process_commands(message)
 
 
-async def exec_command(message: discord.Message):
-    words = message.content.split()
-    command = commands.get(words[0][1:])
-    if command:
-        await command(message, args=words[1:])
-
-
-async def start_game(message: discord.Message, *, args: list[str]):
-    print(f'command: start_game, {args=}')
-    await message.channel.send(f'command: start_game, {args=}')
-
+@bot.command(name='start-game')
+async def start_game(ctx: commands.Context, *args: str):
+    author = ctx.message.author
+    mentions = ctx.message.mentions
+    if author in mentions:
+        return await ctx.send(f"Sorry {author.mention}, you can't play yourself.")
+    if len(mentions) > 1:
+        return await ctx.send(f'Sorry {author.mention}, I currently only support two player chess.')
 
 games: dict[tuple[discord.User | discord.Member], chess.Board] = {}
-commands = {'start-game': start_game}
 
-client.run(token)
+bot.run(token)
